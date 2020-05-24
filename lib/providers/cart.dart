a87980d79a84a8e95e33b90290d3640e6608deae
thilinanimesh <tnimesh77@gmail.com>
2020-05-24 15:36:01 +1000

    Adding Shopping cart function to the MiliCart - still not join with card provider

---
+++ lib/providers/cart.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/foundation.dart';
+
+import './cart_item.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CardItem> _items;
+
+  Map<String, CardItem> get items {
+    return {..._items};
+  }
+
+  void addItem(
+    String productId,
+    double price,
+    String title,
+  ) {
+    if (_items.containsKey(productId)) {
+      //chnage quantity...
+      _items.update(
+        productId,
+        (existingCardItem) => CardItem(
+          id: existingCardItem.id,
+          title: existingCardItem.title,
+          price: existingCardItem.price,
+          quantity: existingCardItem.quantity + 1,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CardItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
