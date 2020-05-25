d7b116adf2d11020d2fb6299dc8b22237f11eccb
thilinanimesh <tnimesh77@gmail.com>
2020-05-25 22:21:09 +1000

    Basic Cart screen design

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    label: Text(''),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
