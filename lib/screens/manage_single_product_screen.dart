f4903d059926597b3b12ae0085cb7047c2a1d21c
thilinanimesh <tnimesh77@gmail.com>
2020-06-11 00:24:20 +1000

    Adding preview fro URL and add focus update to it

--- lib/screens/manage_single_product_screen.dart
+++ lib/screens/manage_single_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ManageSingleProductScreen extends StatefulWidget {
   static const routeName = '/manage-single-product';
@@ -10,12 +11,35 @@ class ManageSingleProductScreen extends StatefulWidget {
 class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
 
   @override
-  void dispose(){
+  void dispose() {
+
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.dispose();
+    _imageUrlController.dispose();
+
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void _updateImageUrl(){
+    if(!_imageUrlFocusNode.hasFocus){
+      setState(() {
+        
+      });
+    }
   }
 
   @override
@@ -41,7 +65,7 @@ class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
-                onFieldSubmitted: (_){
+                onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
               ),
@@ -50,7 +74,42 @@ class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
-              ),              
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      width: 1,
+                      color: Theme.of(context).accentColor,
+                    )),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Please Enter an URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _imageUrlController.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Image URL'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
