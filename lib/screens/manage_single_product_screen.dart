179e7247be5743f14d37b34cc6c5f74ec37fe6a5
thilinanimesh <tnimesh77@gmail.com>
2020-06-12 07:50:24 +1000

    Manae product Save draft // this is a debug stage

--- lib/screens/manage_single_product_screen.dart
+++ lib/screens/manage_single_product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../providers/product.dart';
+
 class ManageSingleProductScreen extends StatefulWidget {
   static const routeName = '/manage-single-product';
   @override
@@ -13,10 +15,18 @@ class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageUrlFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+
+  var _managedProduct = Product(
+    id: null,
+    title: '',
+    description: '',
+    price: 0,
+    imageUrl: '',
+  );
 
   @override
   void dispose() {
-
     _imageUrlFocusNode.removeListener(_updateImageUrl);
 
     _priceFocusNode.dispose();
@@ -34,12 +44,20 @@ class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
     super.initState();
   }
 
-  void _updateImageUrl(){
-    if(!_imageUrlFocusNode.hasFocus){
-      setState(() {
-        
-      });
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
     }
+  }
+
+  void _saveForm() {
+    _form.currentState.save();
+
+    //debug
+    print(_managedProduct.title);
+    print(_managedProduct.price);
+    print(_managedProduct.description);
+    print(_managedProduct.imageUrl);
   }
 
   @override
@@ -47,10 +65,17 @@ class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Manage Single Product'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _saveForm,
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _form,
           child: ListView(
             children: <Widget>[
               TextFormField(
@@ -58,6 +83,15 @@ class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                onSaved: (value) {
+                  _managedProduct = Product(
+                    title: value,
+                    id: null,
+                    description: _managedProduct.description,
+                    price: _managedProduct.price,
+                    imageUrl: _managedProduct.imageUrl,
+                  );
                 },
               ),
               TextFormField(
@@ -68,12 +102,30 @@ class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
+                onSaved: (value) {
+                  _managedProduct = Product(
+                    title: _managedProduct.title,
+                    id: null,
+                    description: _managedProduct.description,
+                    price: double.parse(value),
+                    imageUrl: _managedProduct.imageUrl,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Description'),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
+                onSaved: (value) {
+                  _managedProduct = Product(
+                    title: _managedProduct.title,
+                    id: null,
+                    description: value,
+                    price: _managedProduct.price,
+                    imageUrl: _managedProduct.imageUrl,
+                  );
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -106,6 +158,18 @@ class _ManageSingleProductScreenState extends State<ManageSingleProductScreen> {
                       textInputAction: TextInputAction.done,
                       controller: _imageUrlController,
                       focusNode: _imageUrlFocusNode,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                      onSaved: (value) {
+                        _managedProduct = Product(
+                          title: _managedProduct.title,
+                          id: null,
+                          description: _managedProduct.description,
+                          price: _managedProduct.price,
+                          imageUrl: value,
+                        );
+                      },
                     ),
                   )
                 ],
